7329ea050ec02c404c3f8539db893315909eea9f
Abdushukur <74373255+AbdushukurMaxmudov@users.noreply.github.com>
2021-04-26 15:06:46 +0500

    Add files via upload

---
+++ newPortfolio/js/main.js
@@ -0,0 +1,12 @@
+// Typed.js 
+
+if ($(".text-slider").length == 1) { 
+              
+    var typed = new Typed(".text-slider", { 
+        strings: ["I am A Freelancer.", "I am A Frontend Developer", "I Design Websites.", "I Live in Uzbekistan."], 
+        typeSpeed: 50, 
+        // loop: true, 
+        // backDelay: 900, 
+         backSpeed: 50, 
+    }); 
+}
